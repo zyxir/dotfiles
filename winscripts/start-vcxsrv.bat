3028e68cc448a4b21ba4b187cb906b9718a45d90
Eric Zhuo Chen <zyxirchen@outlook.com>
2022-09-29 18:17:46 +0800

    modify WSL scripts to work with WSL2

--- winscripts/start-vcxsrv.bat
+++ winscripts/start-vcxsrv.bat
@@ -1,3 +1,3 @@
 @REM 此腳本用於啓動 VcXsrv，從而方便運行圖形化 Linux 應用程序。
 
-start "" "C:\Program Files\VcXsrv\vcxsrv.exe" :0 -multiwindow -clipboard -wgl
+start "" "C:\Program Files\VcXsrv\vcxsrv.exe" :0 -multiwindow -clipboard -wgl -ac
---
+++ winscripts/wsl2-emacs.bat
@@ -0,0 +1,3 @@
+@REM 此腳本用於啓動 WSL (Ubuntu-20.04) 中的 Emacs。
+
+start "" wsl -d Ubuntu-20.04 bash -c "export DISPLAY=$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0.0 && export LIBGL_ALWAYS_INDIRECT=1 && setsid emacsclient -c -a emacs"
