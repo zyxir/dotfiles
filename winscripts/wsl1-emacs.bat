4fbadfeefa1c36a0da82407b9a93ff8b2fad13d5
Eric Zhuo Chen <zyxirchen@outlook.com>
2022-11-14 22:37:01 +0800

    Restructure the repository

--- winscripts/wsl1-emacs.bat
+++ winscripts/wsl1-emacs.bat
@@ -1,3 +1,3 @@
 @REM 此腳本用於啓動 WSL (Ubuntu-20.04) 中的 Emacs。
 
-start "" wsl -d Ubuntu-20.04 bash -c "export DISPLAY=127.0.0.1:0.0 && export LIBGL_ALWAYS_INDIRECT=1 && setsid emacsclient -c -a emacs"
+start "" wsl -d Ubuntu bash -c "export DISPLAY=127.0.0.1:0.0 && export LIBGL_ALWAYS_INDIRECT=1 && setsid emacsclient -c -a emacs"
