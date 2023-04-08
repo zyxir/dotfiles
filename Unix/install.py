de66ff917f5e239dea360a67cab93bce53ea529d
Eric Zhuo Chen <zyxirchen@outlook.com>
2023-04-08 18:53:04 +0800

    Write installation script with Python

---
+++ Unix/install.py
@@ -0,0 +1,136 @@
+#!/bin/python3
+"""Cross-platform installation script for my dotfiles."""
+
+import os
+import platform
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+
+class DirectoryError(Exception):
+    """Raised when the script cannot be executed in the correct directory."""
+
+    pass
+
+
+def setup_directory():
+    """Change directory to where this script is in.
+
+    If __file__ is available, use the directory it is in.
+
+    Otherwise, try to find a directory named "Unix" with a "install.py" inside,
+    and change directory to it.  If no directory is found, raise an error.
+    """
+    if "__file__" in globals():
+        os.chdir(Path(__file__).parent)
+    else:
+        candidates = list(Path.cwd().rglob("Unix/install.py"))
+        if len(candidates) == 0:
+            raise DirectoryError
+        else:
+            os.chdir(candidates[0].parent)
+
+
+def copy(src: str, dst: str, dry: bool):
+    """Copy src to dst, printing details.
+
+    Work on a file or a directory.  Expand "~" to home directory.  Overwrite
+    existing file(s).  Create directories if needed.
+
+    If dry is True, perform a dry run.
+    """
+    # Convert to Path objects.
+    src_path = Path(src).expanduser().absolute()
+    dst_path = Path(dst).expanduser().absolute()
+
+    # Raise error if src does not exist.
+    if not src_path.exists():
+        raise FileNotFoundError
+
+    # Copy a single file.
+    if src_path.is_file():
+        if not dst_path.parent.exists():
+            dst_path.parent.mkdir()
+        print(f'Copying "{src_path}" to "{dst_path}".')
+        if not dry:
+            shutil.copy(src_path, dst_path)
+    # Copy a directory recursively.
+    if src_path.is_dir():
+        print(f'Copying "{src_path}" to "{dst_path}" recursively.')
+        if not dry:
+            shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
+
+
+def dconf_load(src: str, dst: str, dry: bool):
+    """Load dconf configuration from src to dst.
+
+    If dry is True, perform a dry run.
+
+    Return the return code of the dconf process, or 0 if dry is True.
+    """
+    # Convert src to Path object.
+    src_path = Path(src).expanduser().absolute()
+
+    # Raise error if src does not exist.
+    if not src_path.exists():
+        raise FileNotFoundError
+
+    # Load with dconf.
+    print(f'Loading "{src_path}" to dconf path "{dst}".')
+    if dry:
+        return 0
+    with open(src_path, "rb") as src_fd:
+        cmd = ["dconf", "load", dst]
+        proc = subprocess.run(cmd, stdin=src_fd)
+        return proc.returncode
+
+
+if __name__ == "__main__":
+    # Parse arguments.
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Installation script for Unix dotfiles."
+    )
+    parser.add_argument("--dry", action="store_true", help="perform a dry run")
+    args = parser.parse_args()
+    dry = args.dry
+
+    # OS constants.
+    LINUX = platform.system() == "Linux"
+
+    # Make sure the script is run in the correct directory.
+    try:
+        setup_directory()
+    except DirectoryError:
+        print("The script cannot find the correct directory to run.")
+        sys.exit(1)
+
+    # Install dot files.
+    copy("dot-gitconfig", "~/.gitconfig", dry)
+    copy("mypy", "~/.config/mypy", dry)
+    if LINUX:
+        copy("dot-bashrc", "~/.bashrc", dry)
+        copy("dot-bash_profile", "~/.bash_profile", dry)
+        copy("fonts.conf", "~/.config/fontconfig/fonts.conf", dry)
+        copy("fcitx5", "~/.config/fcitx5", dry)
+        copy("xremap", "~/.config/xremap", dry)
+
+    # Load dconf.
+    if LINUX and shutil.which("dconf"):
+        dconf_load("gnome-dconf/wm.dconf", "/org/gnome/desktop/wm/", dry)
+        dconf_load("gnome-dconf/mutter.dconf", "/org/gnome/mutter/", dry)
+        dconf_load(
+            "gnome-dconf/media-keys.dconf",
+            "/org/gnome/settings-daemon/media-keys/",
+            dry,
+        )
+        dconf_load(
+            "gnome-dconf/dash-to-dock.dconf",
+            "/org/gnome/shell/extensions/dash-to-dock/",
+            dry,
+        )
+    else:
+        print("Skipping dconf configuration.")
