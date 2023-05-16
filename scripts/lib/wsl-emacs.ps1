c43a0c9ae2b9f16842c50c51852446b2182225f0
Eric Zhuo Chen <zyxirchen@outlook.com>
2023-05-16 10:33:05 +0800

    Fix WSL Emacs script to run on VcXsrv

--- scripts/lib/wsl-emacs.ps1
+++ scripts/lib/wsl-emacs.ps1
@@ -1,1 +1,3 @@
-wsl bash --login -c "setsid emacs"
+$wslip = wsl bash -c "cat /etc/resolv.conf | grep -oP '(?<=nameserver\ ).*'"
+
+wsl bash --login -c "export DISPLAY=$wslip`:0.0 export LIBGL_ALWAYS_INDIRECT=1 && setsid emacs"
