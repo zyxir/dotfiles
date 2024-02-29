841881ffc64b122130ce81d0e203a630afdf41c5
Eric Zhuo Chen <zyxirchen@outlook.com>
2024-02-29 10:41:50 +0800

    Update WSL Emacs script to make it work

--- scripts/lib/wsl-emacs.ps1
+++ scripts/lib/wsl-emacs.ps1
@@ -1,2 +1,2 @@
 $wslip = wsl bash -c "ip route show default" | %{ $_.Split(' ')[2]; }
-wsl bash --login -c "export DISPLAY=$wslip`:0.0 export LIBGL_ALWAYS_INDIRECT=1 && setsid emacs"
+wsl zsh -c "export DISPLAY=$wslip`:0.0 export LIBGL_ALWAYS_INDIRECT=1 && setsid emacs"
