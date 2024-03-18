297aa3b59cb0ecad77ddc4faaed0c868513cd780
Eric Zhuo Chen <ericzhuochen@outlook.com>
2024-03-18 20:25:26 +0800

    Updated

--- apps/nix/home-manager/home.nix
+++ apps/nix/home-manager/home.nix
@@ -1,120 +1,123 @@
 { config, pkgs, ... }:
 
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "zyxir";
-  home.homeDirectory = "/home/zyxir";
+let
+  emacsWithPackages = pkgs.emacsWithPackages (
+    epkgs: with epkgs; [
+      # Put important and hard-to-build packages here.
+      auctex
+      evil
+      org
+      pdf-tools
+      rime
+    ]
+  );
+  in
+  {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = "zyxir";
+    home.homeDirectory = "/home/zyxir";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    home.packages = with pkgs; [
+      # # Adds the 'hello' command to your environment. It prints a friendly
+      # # "Hello, world!" when run.
+      # pkgs.hello
 
-    # Command line utilities.
-    curl
-    fd
-    fontconfig
-    git
-    p7zip
-    ripgrep
-    vim
-    zip
+      # Command line utilities.
+      curl
+      fd
+      fontconfig
+      git
+      p7zip
+      ripgrep
+      vim
+      zip
 
-    # Running Python scripts.
-    (python3.withPackages (python-pkgs: with python-pkgs; [
-      isort
-      matplotlib
-      numpy
-      pandas
-      scipy
-    ]))
-    black
-    black-macchiato
-    nodePackages.pyright
+      # Python and accessories.
+      (python3.withPackages (python-pkgs: with python-pkgs; [
+        isort
+        matplotlib
+        numpy
+        pandas
+        scipy
+      ]))
+      black
+      black-macchiato
+      nodePackages.pyright
 
-    # Emacs with packages.
-    ((emacsPackagesFor emacs).emacsWithPackages (
-      epkgs: with epkgs; [
-        # Add packages here so that they are already compiled while using Emacs.
-        pdf-tools
-      ]
-    ))
-  ];
+      # Emacs.
+      emacsWithPackages
+      rime-data
+    ];
 
-  # Setup nix-direnv.
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    nix-direnv.enable = true;
-  };
-
-  # Setup Emacs daemon service.
-  services.emacs = {
-    enable = true;
-    # This should be the same as above.
-    package = with pkgs; (emacsPackagesFor emacs).emacsWithPackages (
-      epkgs: with epkgs; [
-        pdf-tools
-      ]
-    );
-  };
-
-  # Git config.
-  programs.git = {
-    enable = true;
-    userName = "Eric Zhuo Chen";
-    userEmail = "ericzhuochen@outlook.com";
-    extraConfig = {
-      credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+    # Setup nix-direnv.
+    programs.direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
     };
-  };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # Setup Emacs daemon service.
+    services.emacs = {
+      enable = true;
+      package =  emacsWithPackages;
+    };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+    # Git config.
+    programs.git = {
+      enable = true;
+      userName = "Eric Zhuo Chen";
+      userEmail = "ericzhuochen@outlook.com";
+      extraConfig = {
+        credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+      };
+    };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/zyxir/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    home.file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-}
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
+
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. If you don't want to manage your shell through Home
+    # Manager then you have to manually source 'hm-session-vars.sh' located at
+    # either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/zyxir/etc/profile.d/hm-session-vars.sh
+    #
+    home.sessionVariables = {
+      # EDITOR = "emacs";
+    };
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+  }
