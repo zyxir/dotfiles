a80a303efcefd0d74c0aa9331ee28f207611a6e0
Eric Zhuo Chen <zyxirchen@outlook.com>
2024-04-12 22:39:07 +0800

    Update build script

---
+++ subprojects/installer/build.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""This script build the installer package into a single .pyz file, put it at
+the root of the dotfiles repository, and remove all temporary files.
+"""
+
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+# The directory of this script.
+curdir = Path(__file__).parent
+
+# Compile to a single file.
+if not shutil.which("shiv"):
+    print("'shiv' is not available.")
+    sys.exit(1)
+else:
+    cmd = "shiv -c installer -o ../../install.pyz ."
+    subprocess.run(cmd.split(), cwd=curdir)
+
+# Remove temporary files.
+shutil.rmtree(curdir.joinpath("build"))
+shutil.rmtree(curdir.joinpath("installer.egg-info"))
